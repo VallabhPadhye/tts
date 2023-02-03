93446bbe9aeeb41ef8fa2b2ce07875280c588d41
Vallabh Padhye <54024656+VallabhPadhye@users.noreply.github.com>
2023-02-03 10:33:10 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -5,11 +5,12 @@ st.set_page_config(page_title="Text to Speech Converter",
                   page_icon=":microphone:",
                   layout="wide")
 
-def tts(text):
-    tts = gTTS(text=text, lang='en')
+def tts(text, voice):
+    tts = gTTS(text=text, lang='en', gender=voice)
     tts.save("output.mp3")
 
 text = st.text_area("Enter text here:")
+voice = st.selectbox("Choose voice:", ["Male", "Female"])
 if st.button("Convert to Speech"):
-    tts(text)
+    tts(text, voice.lower()[0])
     st.audio("output.mp3", format="audio/mp3")
