344c6544728bd1122ab1cf70058e759c1e513f4e
Vallabh Padhye <54024656+VallabhPadhye@users.noreply.github.com>
2023-02-03 10:40:40 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -5,12 +5,11 @@ st.set_page_config(page_title="Text to Speech Converter",
                   page_icon=":microphone:",
                   layout="wide")
 
-def tts(text, lang):
-    tts = gTTS(text=text, lang=lang)
+def tts(text):
+    tts = gTTS(text=text, lang='en')
     tts.save("output.mp3")
 
 text = st.text_area("Enter text here:")
-lang = st.selectbox("Choose language:", ["English", "Hindi", "Spanish"])
 if st.button("Convert to Speech"):
-    tts(text, lang.lower())
+    tts(text)
     st.audio("output.mp3", format="audio/mp3")
