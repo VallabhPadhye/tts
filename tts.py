bffd9d284ae07a149d442bfe676e4a29eb2fbfbf
Vallabh Padhye <54024656+VallabhPadhye@users.noreply.github.com>
2023-02-03 10:31:33 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,21 +1,15 @@
 import streamlit as st
-import pyttsx3
+from gtts import gTTS
 
 st.set_page_config(page_title="Text to Speech Converter",
                   page_icon=":microphone:",
                   layout="wide")
 
-engine = pyttsx3.init()
-
-def tts(text, voice):
-    if voice == 'Male':
-        engine.setProperty('voice', 'male')
-    else:
-        engine.setProperty('voice', 'female')
-    engine.say(text)
-    engine.runAndWait()
+def tts(text):
+    tts = gTTS(text=text, lang='en')
+    tts.save("output.mp3")
 
 text = st.text_area("Enter text here:")
-voice = st.selectbox("Select Voice:", ["Male", "Female"])
 if st.button("Convert to Speech"):
-    tts(text, voice)
+    tts(text)
+    st.audio("output.mp3", format="audio/mp3")
